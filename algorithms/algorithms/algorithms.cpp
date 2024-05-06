7d441282e83095e147cebc88d0af481cadd39122
AFeiYA <386665678@qq.com>
2024-05-07 00:08:54 +0800

    head and tail recursion

--- algorithms/algorithms/algorithms.cpp
+++ algorithms/algorithms/algorithms.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+using namespace std;
+// head recursion
+void fun1(int n)
+{
+	if (n > 0)
+	{
+		fun1(n - 1);
+		std::cout << n << " ";
+	}
+}
 
+// tail recursion
+void fun2(int n)
+{
+	if (n > 0)
+	{
+		std::cout << n << " ";
+		fun2(n - 1);
+	}
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout<<"Head Recursion: ";
+	fun1(3);	
+	cout<<endl;
+	// 1 2 3
+	cout<<"Tail Recursion: ";
+	fun2(3);
+	// 3 2 1
     return 0;
 }
