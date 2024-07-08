0655863546dffb6feed3c19925fa6dcc0b677a72
AFeiYA <386665678@qq.com>
2024-07-08 22:13:35 +0800

    circular queue

--- algorithms/queue_using_array/queue_using_array.cpp
+++ algorithms/queue_using_array/queue_using_array.cpp
@@ -1,7 +1,5 @@
 
-// What is Queue?
-// Queue is a linear data structure that stores items in First In First Out (FIFO) manner.
-// A good example of queue is any queue of consumers for a resource where the consumer that came first is served first.
+
 #include <iostream>
 class Queue {
 private:
@@ -51,18 +49,74 @@ void Queue::Display() {
 	std::cout << std::endl;
 }
 
+class CircularQueue {
+	private:
+		int size;
+		int front;
+		int rear;
+		int* Q;
+	public:
+		CircularQueue() {
+			front = rear = 0;
+			size = 10;
+			Q = new int[size];
+		}
+		CircularQueue(int size) {
+			front = rear = 0;
+			this->size = size;
+			Q = new int[this->size];
+		}
+		void enqueue(int x);
+		int dequeue();
+		void Display();
+};
 
-
+void CircularQueue::enqueue(int x) {
+	if ((rear + 1) % size == front) {
+		std::cout << "Queue is full" << std::endl;
+	}
+	else {
+		rear = (rear + 1) % size;
+		Q[rear] = x;
+	}
+}
+int CircularQueue::dequeue() {
+	int x = -1;
+	if (front == rear) {
+		std::cout << "Queue is empty" << std::endl;
+	}
+	else {
+		front = (front + 1) % size;
+		x = Q[front];
+	}
+	return x;
+}
+void CircularQueue::Display() {
+	int i = front + 1;
+	do {
+		std::cout << Q[i] << " ";
+		i = (i + 1) % size;
+	} while (i != (rear + 1) % size);
+	std::cout << std::endl;
+}
 
 int main()
 {
-    Queue q(5);
+    CircularQueue q(5);
 	q.enqueue(10);
 	q.enqueue(20);
 	q.enqueue(30);
 	q.enqueue(40);
 	q.enqueue(50);
 	q.Display();
+	q.dequeue();
+	q.Display();
+	q.dequeue();
+	q.Display();
 	q.enqueue(60);
+	q.Display();
+	q.enqueue(70);
+	q.Display();
+
 	return 0;
 }
