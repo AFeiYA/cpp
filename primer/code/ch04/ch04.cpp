b14ebbc71a48cd2efb8eaf96f50533135c96946f
AFeiYA <386665678@qq.com>
2024-09-26 23:23:19 +0800

    ch05

---
+++ primer/code/ch04/ch04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "../ch01/Sales_item.h"
+using namespace std;
+
+
+int main()
+{
+	//arithmetics
+
+	cout << "Hello CMake CH04." << endl;
+	return 0;
+}
